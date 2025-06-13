75ca4899f8566e89230f400a5b979f54ca62d99a
Isabel <marisahfz07@gmail.com>
2025-06-13 00:56:32 -0600

    Changes to the Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,22 @@
+import { UserCircle } from "lucide-react";
+
 const Header = () => {
   return (
-    <header className="mb-8">
-      <nav className="flex space-x-6 text-lg font-medium">
-        <a href="#" className="text-blue-600 border-b-2 border-blue-600 pb-2">Gestion de usuarios</a>
-        <a href="#" className="text-gray-600 hover:text-blue-600 pb-2">Membresias</a>
-        <a href="#" className="text-gray-600 hover:text-blue-600 pb-2">Control de acceso</a>
-      </nav>
+    <header className="fixed top-0 left-0 right-0 z-50 px-8 py-10 bg-gray-900">
+      <div className="flex items-center justify-between text-lg font-medium">
+        <div className="flex items-center space-x-2">
+          <img src="/Logo.png" alt="Logo" className="h-10 w-auto" />
+        </div>
+        
+        <div className="flex space-x-6 text-lg font-medium">
+          <a href="#" className="text-white border-b-2 border-yellow-500 pb-2 hover:text-yellow-400 transition-colors">
+            Inicio
+          </a>
+          <UserCircle className="h-8 w-8 text-white" />
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
