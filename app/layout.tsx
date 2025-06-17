1c59596c38043f9b36dc2c7c525d93582a5cf020
Isabel <marisahfz07@gmail.com>
2025-06-16 19:08:25 -0600

    Create a Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,22 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Oswald, Roboto } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// Components
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const oswald = Oswald({ 
+  subsets: ['latin'], 
+  weight: ['200', '300', '400', '500', '600', '700'],
+  variable: '--font-oswald',
+})
+
+const roboto = Roboto({ 
+  subsets: ['latin'], 
+  weight: ['100', '300', '400', '500', '700', '900'],
+  variable: '--font-roboto',
+})
 
 export const metadata: Metadata = {
   title: 'GymGG',
@@ -16,8 +30,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={`${inter.className} bg-gray-100`}>
+      <body className={`${oswald.variable} ${roboto.variable}`}>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
